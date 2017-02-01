d71f4108c288b62dc180fa648e0553ba3b3d78fc
Agustin <fernandezlado@gmail.com>
2017-02-01 18:54:14 +0000

    First trial FarInteractions

--- src/main.f90
+++ src/main.f90
@@ -8,12 +8,15 @@ PROGRAM MultipleParticle
   IMPLICIT NONE
 
   INTEGER::j,l
-  type(Obstacle)::obs
+  type(Obstacle)::obs1,obs2
   type(intOperator)::op
-
-  CALL createObstacle ( obs , 1, (/ 1.0d0,0.0d0,0.0d0 /), 32)
-  CALL createOperator ( op, obs, (/ .TRUE. , .TRUE. , .FALSE. , .FALSE. /), 1.0d0, 1.0d0 )
-
+  type(FarInteractions)::farInter_obs1,farInter_obs2
+  
+  CALL createObstacle ( obs_1 , 1, (/ 1.0d0 , 0.0d0 , 0.0d0 /), 32)
+  CALL createObstacle ( obs_2 , 1, (/ 2.0d0 , 5.0d0 , 5.0d0 /), 32)  
+!  CALL createOperator ( op, obs, (/ .TRUE. , .TRUE. , .FALSE. , .FALSE. /), 1.0d0, 1.0d0 )
+  CALL createFarInteractions ( farInter_obs1 , obs_1, (/ obs_2 /), 1.0d0)
+  CALL createFarInteractions ( farInter_obs2 , obs_2, (/ obs_1 /), 1.0d0)
 
   CALL destroyObstacle(obs)
   CALL destroyOperator(op)
